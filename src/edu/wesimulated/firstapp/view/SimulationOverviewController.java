19f30623d93cca79becc78d5fa4e53031617cf03
Carolina Soledad Galli <carogalli@gmail.com>
2016-01-24 22:44:50 +0100

    The time of the log cames from the reflect attributes method

--- src/edu/wesimulated/firstapp/view/SimulationOverviewController.java
+++ src/edu/wesimulated/firstapp/view/SimulationOverviewController.java
@@ -1,8 +1,7 @@
 package edu.wesimulated.firstapp.view;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
@@ -17,17 +16,17 @@ public class SimulationOverviewController {
 	@FXML
 	private TextArea textDisplay;
 	private MainApp mainApp;
-	
-	public SimulationOverviewController () {
+
+	public SimulationOverviewController() {
 		Simulation.getInstance().setSimulationOverviewController(this);
 	}
-	
+
 	@FXML
 	private void initialize() {
 		this.textDisplay.setEditable(false);
 		this.textDisplay.setText("Log: \n");
 	}
-	
+
 	@FXML
 	private void loadPeopleAndTasks() {
 		for (Person person : this.mainApp.getPersonData()) {
@@ -38,28 +37,32 @@ public class SimulationOverviewController {
 		}
 		Simulation.getInstance().registerLogger();
 	}
-	
+
 	@FXML
 	private void stopAndDestroySimulation() {
 		Simulation.getInstance().destroyFederation();
 	}
-	
+
 	@FXML
 	private void startSimulation() {
 		Simulation.getInstance().startFederation();
 	}
-	
-	public void log(String message) {
-		this.textDisplay.appendText(this.getTimeToLog() + " " + message + "\n");
+
+	public void log(String message, Date date) {
+		this.textDisplay.appendText(this.getTimeToLog(date) + " " + message + "\n");
 	}
 
-	public void log(float workDone) {
-		this.textDisplay.appendText(this.getTimeToLog() + " " + new LogEvent(workDone) + "\n");
+	public void log(float workDone, Date date) {
+		this.textDisplay.appendText(this.getTimeToLog(date) + " " + new LogEvent(workDone) + "\n");
 	}
-	
-	private String getTimeToLog() {
-		LocalDateTime now = LocalDateTime.now();
-		return now.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL));
+
+	private String getTimeToLog(Date date) {
+		Date logDate = date;
+		if (date == null) {
+			logDate = new Date();
+		}
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd  hh:mm");
+		return formatter.format(logDate);
 	}
 
 	public void setMainApp(MainApp mainApp) {
