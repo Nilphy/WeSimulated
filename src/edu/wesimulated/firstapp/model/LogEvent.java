790274ca512daebcbf8edb8fd1e4c8eb71781171
Carolina Soledad Galli <carogalli@gmail.com>
2016-01-24 22:39:11 +0100

    The date was already logged in the simulation overview controller

--- src/edu/wesimulated/firstapp/model/LogEvent.java
+++ src/edu/wesimulated/firstapp/model/LogEvent.java
@@ -1,17 +1,10 @@
 package edu.wesimulated.firstapp.model;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class LogEvent {
 	
-	private static final DateTimeFormatter DATE_TIME_FORMATER = DateTimeFormatter.ofPattern("d/m/Y H:M:S.N");
-	private ObjectProperty<LocalDateTime> receivedMessageDate;
 	private StringProperty event;
 
 	public LogEvent() {
@@ -19,12 +12,11 @@ public class LogEvent {
     }
 	
 	public LogEvent(double unitsOfWork) {
-		this.event = new SimpleStringProperty(String.format("Person has done %f units of work", unitsOfWork));
-		this.receivedMessageDate =  new SimpleObjectProperty<LocalDateTime>(LocalDateTime.now());
+		this.event = new SimpleStringProperty(String.format("Person has done a total of %f units of work", unitsOfWork));
 	}
 
 	@Override
 	public String toString() {
-		return DATE_TIME_FORMATER.format(receivedMessageDate.getValue()) + ": " + event.getValue();
+		return event.getValue();
 	}
 }
