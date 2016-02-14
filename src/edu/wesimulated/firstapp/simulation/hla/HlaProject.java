aae8307b928fbf3c2e0e41e1dd9cef44c5127520
Carolina Soledad Galli <carogalli@gmail.com>
2016-02-14 03:16:40 +0100

    Add new HLA classes for new simulators

---
+++ src/edu/wesimulated/firstapp/simulation/hla/HlaProject.java
@@ -0,0 +1,14 @@
+package edu.wesimulated.firstapp.simulation.hla;
+
+import hla.rti1516e.ObjectClassHandle;
+import hla.rti1516e.ObjectInstanceHandle;
+import hla.rti1516e.RTIambassador;
+
+public class HlaProject extends HlaObject {
+
+	public HlaProject(RTIambassador rtiAmbassador, ObjectClassHandle classHandle, ObjectInstanceHandle personHandle, String personName) {
+		super(rtiAmbassador, classHandle, personHandle, personName);
+		// TODO Auto-generated constructor stub
+	}
+
+}
