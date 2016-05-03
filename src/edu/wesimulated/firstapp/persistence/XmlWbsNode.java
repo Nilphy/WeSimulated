203bf7d8eb961480d9443c42e3adb7a5da5176d3
Carolina Soledad Galli <carogalli@gmail.com>
2016-05-03 02:44:30 +0200

    code style autoformat

--- src/edu/wesimulated/firstapp/persistence/XmlWbsNode.java
+++ src/edu/wesimulated/firstapp/persistence/XmlWbsNode.java
@@ -9,7 +9,7 @@ public class XmlWbsNode {
 	private Integer taskId;
 	private String name;
 	private Collection<XmlWbsNode> children;
-	
+
 	public void populateFromWbsNodeData(WbsNode wbsNode) {
 		wbsNode.fillXmlNode(this);
 	}
@@ -17,27 +17,27 @@ public class XmlWbsNode {
 	public boolean isLeaf() {
 		return this.taskId != null;
 	}
-	
+
 	public Integer getTaskId() {
 		return taskId;
 	}
-	
+
 	public void setTaskId(Integer taskId) {
 		this.taskId = taskId;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public Collection<XmlWbsNode> getChildren() {
 		return children;
 	}
-	
+
 	public void setChildren(Collection<XmlWbsNode> children) {
 		this.children = children;
 	}
