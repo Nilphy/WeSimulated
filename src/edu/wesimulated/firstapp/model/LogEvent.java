86f6c1823a37fe03e74a561680fd5dee82f1b5a6
Carolina Soledad Galli <carogalli@gmail.com>
2016-05-08 21:59:49 +0200

    code style

--- src/edu/wesimulated/firstapp/model/LogEvent.java
+++ src/edu/wesimulated/firstapp/model/LogEvent.java
@@ -4,13 +4,13 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class LogEvent {
-	
+
 	private StringProperty event;
 
 	public LogEvent() {
-        this(0);
-    }
-	
+		this(0);
+	}
+
 	public LogEvent(double unitsOfWork) {
 		this.event = new SimpleStringProperty(String.format("Person has done a total of %f units of work", unitsOfWork));
 	}
