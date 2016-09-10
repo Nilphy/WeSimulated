3c4cab4860c89001b6012c3c06c052b5bdfd5551
Carolina Soledad Galli <carogalli@gmail.com>
2016-09-10 23:39:39 +0200

    This class is not abstract by now

--- src/edu/wesimulated/firstapp/simulation/stochastic/var/RandomVar.java
+++ src/edu/wesimulated/firstapp/simulation/stochastic/var/RandomVar.java
@@ -2,7 +2,7 @@ package edu.wesimulated.firstapp.simulation.stochastic.var;
 
 import edu.wesimulated.firstapp.simulation.stochastic.ParametricAlgorithm;
 
-public abstract class RandomVar extends ParametricAlgorithm{
+public class RandomVar extends ParametricAlgorithm {
 
 	public long findRandomSample() {
 		// TODO Auto-generated method stub
