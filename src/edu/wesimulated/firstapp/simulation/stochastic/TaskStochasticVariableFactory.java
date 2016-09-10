f1f01bd34e95b9a52adddd172f540dd3c7e11962
Carolina Soledad Galli <carogalli@gmail.com>
2016-09-10 23:35:42 +0200

    Remove class because it was misspelled

--- src/edu/wesimulated/firstapp/simulation/stochastic/TaskStochasticVariableFactory.java
+++ src/edu/wesimulated/firstapp/simulation/stochastic/TaskStochasticVariableFactory.java
@@ -1,10 +1,31 @@
 package edu.wesimulated.firstapp.simulation.stochastic;
 
+import edu.wesimulated.firstapp.simulation.stochastic.var.RandomVar;
+
 public class TaskStochasticVariableFactory {
 
-	public static TaskStockasticVariableFactory buildFactory() {
+	public static TaskStochasticVariableFactory buildFactory() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	public RandomVar buildTimeToConfigureWorkbench() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RandomVar buildTimeToFocus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RandomVar buildTimeOfWorkSlab() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RandomVar buildUowBugs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
