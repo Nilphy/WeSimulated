33ee599adf6f95a8461834547beb8fa1c814cb02
Carolina Soledad Galli <carogalli@gmail.com>
2016-12-04 22:43:47 +0100

    Remove copy pasted code from Person class

--- src/edu/wesimulated/firstapp/simulation/domain/Role.java
+++ src/edu/wesimulated/firstapp/simulation/domain/Role.java
@@ -1,35 +1,6 @@
 package edu.wesimulated.firstapp.simulation.domain;
 
-import hla.rti1516e.AttributeHandleValueMap;
-import hla.rti1516e.ObjectInstanceHandle;
-import edu.wesimulated.firstapp.simulation.hla.HlaPerson;
 import edu.wesimulated.firstapp.simulation.stochastic.NumericallyModeledEntity;
 
 public class Role implements NumericallyModeledEntity {
-
-	private HlaPerson hlaPerson;
-
-	public String getLastWorkDone() {
-		return this.getHlaPerson().getLastWorkDone();
-	}
-
-	public ObjectInstanceHandle getHlaObjectInstanceHandle() {
-		return this.getHlaPerson().getObjectInstanceHandle();
-	}
-
-	public String getName() {
-		return this.getHlaPerson().getObjectInstanceName();
-	}
-
-	public void reflectAttributeValues(AttributeHandleValueMap attributeValues) {
-		this.hlaPerson.reflectAttributeValues(attributeValues);
-	}
-	
-	public void setHlaPerson(HlaPerson hlaPerson) {
-		this.hlaPerson = hlaPerson;
-	}
-
-	private HlaPerson getHlaPerson() {
-		return hlaPerson;
-	}
- }
+}
