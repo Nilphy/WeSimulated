ac7260eed9f6585a8b8cc0bc8bf837b3976e56a4
Carolina Soledad Galli <carogalli@gmail.com>
2017-01-22 12:45:26 +0100

    Fix load of model object from data objects: Load the config properly and add the name information

--- src/edu/wesimulated/firstapp/simulation/stochastic/StochasticMethodBuilder.java
+++ src/edu/wesimulated/firstapp/simulation/stochastic/StochasticMethodBuilder.java
@@ -6,11 +6,11 @@ import edu.wesimulated.firstapp.simulation.stochastic.method.ConstantProbability
 public class StochasticMethodBuilder {
 
 	public static StochasticMethod fromStochasticVarData(StochasticVarData var) {
+		StochasticMethodConfig config = StochasticMethodConfig.fromStochasticMethodConfigData(var.getConfig());
 		StochasticMehodType type = StochasticMehodType.valueOf(var.getType());
-		StochasticMethodConfig config = new StochasticMethodConfig();
 		switch (type) {
 		case ConstantProbability:
-			return new ConstantProbability(config);
+			return new ConstantProbability(config, var.getName());
 		default:
 			break;
 		}
--- src/edu/wesimulated/firstapp/simulation/stochastic/StochasticMethodConfig.java
+++ src/edu/wesimulated/firstapp/simulation/stochastic/StochasticMethodConfig.java
@@ -1,18 +1,48 @@
 package edu.wesimulated.firstapp.simulation.stochastic;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import edu.wesimulated.firstapp.model.StochasticMethodConfigData;
 
 
 public class StochasticMethodConfig {
 
 	private Map<String, String> values;
 	
+	public StochasticMethodConfig(Map<String, String> values) {
+		this.values = values;
+	}
+	
 	public interface StochasticMethodConfigType {
-		
 		public String name();
 	};
 	
 	public String getValue(StochasticMethodConfigType key) {
 		return values.get(key);
+	}
+	
+	public String getValue(String key) {
+		return values.get(key);
+	}
+
+	public Collection<String> getKeys() {
+		return values.keySet();
+	}
+	
+	public Set<Entry<String, String>> getEntries() {
+		return values.entrySet();
+	}
+
+	public static StochasticMethodConfig fromStochasticMethodConfigData(List<StochasticMethodConfigData> config) {
+		Map<String, String> configValues = new HashMap<>();
+		for (StochasticMethodConfigData stochasticMethodConfigData : config) {
+			configValues.put(stochasticMethodConfigData.getName(), stochasticMethodConfigData.getValue());
+		}
+		return new StochasticMethodConfig(configValues);
 	}
 }
