2730b98df5eca4787ac4a9994e17c34d34c31299
Carolina Soledad Galli <carogalli@gmail.com>
2017-01-22 12:45:26 +0100

    Creating a temporal entry point for stochastic method configurations

---
+++ src/edu/wesimulated/firstapp/simulation/stochastic/StochasticVarConfigCodeUI.java
@@ -0,0 +1,39 @@
+package edu.wesimulated.firstapp.simulation.stochastic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.wesimulated.firstapp.model.StochasticVarData;
+
+/**
+ * This class is created with the intention of having a way to configure the
+ * stochastic methods used by the simulation without needing an UI
+ * 
+ * @author Carolina
+ *
+ */
+public class StochasticVarConfigCodeUI {
+
+	private static StochasticVarConfigCodeUI instance;
+	private Map<StochasticVar, Map<String, String>> configOfVar;
+
+	public static StochasticVarConfigCodeUI getInstance() {
+		if (instance == null) {
+			instance = new StochasticVarConfigCodeUI();
+		}
+		return instance;
+	}
+
+	private StochasticVarConfigCodeUI() {
+		this.configOfVar = new HashMap<StochasticVar, Map<String, String>>();
+		/**
+		 * TODO: Add all stochastic vars config here
+		 */
+		this.configOfVar.put(StochasticVar.TimeOfWorkSlab, this.buildConfigForTimeOfWorkSlab());
+	}
+
+	private Map<String, String> buildConfigForTimeOfWorkSlab() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
