fe2a51bb6b4308f51610f4f087a6b4cd00c0f6e2
Carolina Soledad Galli <carogalli@gmail.com>
2017-04-02 13:06:35 +0200

    Add posibility to store internally the value as a number

--- src/edu/wesimulated/firstapp/simulation/stochastic/EntryValue.java
+++ src/edu/wesimulated/firstapp/simulation/stochastic/EntryValue.java
@@ -1,19 +1,40 @@
 package edu.wesimulated.firstapp.simulation.stochastic;
 
+/**
+ * get Value puede construir un Number y devolver los long value, float value...
+ * y si es una classification ver de construir la correspondiente classification
+ * o devolver el string según corresponda
+ */
 public class EntryValue {
 
-	public enum EntryValueType {
-		Long,
-		Int,
-		Float,
-		String
+	public enum Type {
+		Int, Float, String
 	}
-	
-	private EntryValueType type;
-	private String value;
 
-	/**
-	 * get Value puede construir un Number y devolver los long value, float value...
-	 * y si es una classification ver de construir la correspondiente classification o devolver el string según corresponda
-	 */
+	private Type type;
+	private String stringValue;
+	private Number numberValue;
+
+	public EntryValue(Type type, String value) {
+		this.type = type;
+		this.stringValue = value;
+	}
+
+	public EntryValue(Type type, Number value) {
+		this.type = type;
+		this.numberValue = value;
+	}
+
+	public Number getNumber() {
+		if (this.numberValue != null) {
+			return this.numberValue;
+		}
+		if (this.type == Type.Int) {
+			return Integer.getInteger(this.stringValue);
+		}
+		if (this.type == Type.Float) {
+			return Float.parseFloat(this.stringValue);
+		}
+		throw new IllegalStateException();
+	}
 }
