6439d3a5190e52c52b7511e90353d4823af6e451
Carolina Soledad Galli <carogalli@gmail.com>
2017-07-09 11:53:02 +0200

    Removed highly interruptible role task because this simulator doesn't work that way

--- src/edu/wesimulated/firstapp/simulation/domain/highlyinterruptiblerole/HighlyInterruptibleTask.java
+++
@@ -1,8 +0,0 @@
-package edu.wesimulated.firstapp.simulation.domain.highlyinterruptiblerole;
-
-import edu.wesimulated.firstapp.simulation.domain.Task;
-
-public class HighlyInterruptibleTask extends Task {
-
-	// TODO add specifics needed from highlyInterruptibleRole Simulator
-}
