9b9a73f9c861ada84b0db8ecf75b10ef18625c82
Carolina Soledad Galli <carogalli@gmail.com>
2017-11-02 21:49:08 +0100

    Add characteristics to technology

--- src/edu/wesimulated/firstapp/simulation/domain/Technology.java
+++ src/edu/wesimulated/firstapp/simulation/domain/Technology.java
@@ -1,19 +1,48 @@
 package edu.wesimulated.firstapp.simulation.domain;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import edu.wesimulated.firstapp.simulation.stochastic.EntryValue;
+import edu.wesimulated.firstapp.simulation.stochastic.EntryValue.Type;
 import edu.wesimulated.firstapp.simulation.stochastic.NumericallyModeledEntity;
 
 public class Technology implements NumericallyModeledEntity {
 
-	private Integer monthsInTheIndustry;
+	private Long  monthsInTheIndustry;
 	private Float learningCurveModOne;
 	private Float verbosityModOne;
 
 	@Override
 	public Map<Characteristic, EntryValue> extractValues() {
-		// TODO extractValues
-		return null;
+		Map<Characteristic, EntryValue> out = new HashMap<>();
+		out.put(TechnologyCharacteristic.MONTHS_IN_THE_INDUSTRY, new EntryValue(Type.Long, this.getMonthsInTheIndustry()));
+		out.put(TechnologyCharacteristic.LEARNING_CURVE_MOD_ONE, new EntryValue(Type.Float, this.getLearningCurveModOne()));
+		out.put(TechnologyCharacteristic.VERBOSITY_MOD_ONE, new EntryValue(Type.Float, this.getVerbosityModOne()));
+		return out;
+	}
+
+	public Long getMonthsInTheIndustry() {
+		return monthsInTheIndustry;
+	}
+
+	public void setMonthsInTheIndustry(Long monthsInTheIndustry) {
+		this.monthsInTheIndustry = monthsInTheIndustry;
+	}
+
+	public Float getLearningCurveModOne() {
+		return learningCurveModOne;
+	}
+
+	public void setLearningCurveModOne(Float learningCurveModOne) {
+		this.learningCurveModOne = learningCurveModOne;
+	}
+
+	public Float getVerbosityModOne() {
+		return verbosityModOne;
+	}
+
+	public void setVerbosityModOne(Float verbosityModOne) {
+		this.verbosityModOne = verbosityModOne;
 	}
 }
--- src/edu/wesimulated/firstapp/simulation/domain/TechnologyCharacteristic.java
+++ src/edu/wesimulated/firstapp/simulation/domain/TechnologyCharacteristic.java
@@ -1,5 +1,6 @@
 package edu.wesimulated.firstapp.simulation.domain;
 
 public enum TechnologyCharacteristic implements Characteristic {
+	VERBOSITY_MOD_ONE, LEARNING_CURVE_MOD_ONE, MONTHS_IN_THE_INDUSTRY
 
 }
