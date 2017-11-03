d6707af108cf371f7b241a5f73f2e60ea8694d6f
Carolina Soledad Galli <carogalli@gmail.com>
2017-11-03 18:27:30 +0100

    rename

--- src/edu/wesimulated/firstapp/simulation/domain/mywork/task/SystemDynamicsTask.java
+++
@@ -1,7 +0,0 @@
-package edu.wesimulated.firstapp.simulation.domain.mywork.task;
-
-import edu.wesimulated.firstapp.simulation.domain.Task;
-
-public class SystemDynamicsTask extends Task {
-
-}
---
+++ src/edu/wesimulated/firstapp/simulation/domain/mywork/task/Task.java
@@ -0,0 +1,5 @@
+package edu.wesimulated.firstapp.simulation.domain.mywork.task;
+
+public class Task extends edu.wesimulated.firstapp.simulation.domain.Task {
+
+}
